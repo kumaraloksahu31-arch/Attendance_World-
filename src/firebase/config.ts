5f85bf0da67468a1c07b14a6c25bb107e7848652
Alok Sahu <kumaraloksahu31@gmail.com>
2025-12-04 13:35:10 +0000

    ofter deploy the webside show this

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,10 +7,11 @@
 // in the provider.
 
 export const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    apiKey: "your_firebase_api_key_here",
     authDomain: "studio-4899358888-9bd13.firebaseapp.com",
     projectId: "studio-4899358888-9bd13",
     storageBucket: "studio-4899358888-9bd13.appspot.com",
     messagingSenderId: "549477142818",
     appId: "1:549477142818:web:ba8adff74b42da70cf957b"
 };
+
